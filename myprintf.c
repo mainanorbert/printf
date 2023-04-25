6cbc7ef48060ebf7ebd52277c00c9f2f8b1fdb86
mainanorbert <mainanorbert@gmail.com>
2023-04-25 19:19:06 +0000

    added some defenses

--- myprintf.c
+++ myprintf.c
@@ -14,7 +14,7 @@ int _printf(const char *format, ...)
 	va_start(args, format);
 	if (!format[i])
 		return (-1);
-	if ((format[i] == '%') && (format[i + 1]) == '\0')
+	if ((format[i] == '%') && (!format[i + 1]))
 		return (-1);
 	while (format[i])
 	{
@@ -37,5 +37,6 @@ int _printf(const char *format, ...)
 		i++;
 	}
 	va_end(args);
-	return (len - 1);
+	len = len - 1;
+	return (len);
 }
