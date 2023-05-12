003072d7ee34fd9f1e07ac94e5cda630259cff72
mainanorbert <mainanorbert@gmail.com>
2023-05-12 11:34:54 +0000

    printing %d and %i

--- other_functions.c
+++ other_functions.c
@@ -1,25 +1,34 @@
 #include "main.h"
 /**
  * print_decimal - prints decimal
- * @num: prints a number
+ * @myNum: prints a number
  * Return: count
  */
 
-int print_decimal(int num)
+int print_decimal(int myNum)
 {
 	int count = 0;
 
-	if (num < 0)
+	if (myNum < 0)
 	{
-		_putchar('-');
-		count++;
-		num = -num;
+		if (myNum == INT_MIN)
+		{
+			_putchar('-');
+			count++;
+			myNum = -(myNum + 1);
+		}
+		else
+		{
+			_putchar('-');
+			count++;
+			myNum = -myNum;
+		}
 	}
-	if (num >= 10)
+	if (myNum >= 10)
 	{
-		count += print_decimal(num / 10);
+		count += print_decimal(myNum / 10);
 	}
-	_putchar(num % 10 + '0');
+	_putchar(myNum % 10 + '0');
 	count++;
 	return (count);
 }
